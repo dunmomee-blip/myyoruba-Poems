e51b8f3438eea95767e5a4a428bbe6af0b84ca17
dunmomee-blip <raafiah.oladunmomi@gmail.com>
2026-03-28 04:08:27 -0700

    improved User Experience

--- src/index.js
+++ src/index.js
@@ -14,9 +14,13 @@ function createPoem(event) {
 
   let apiKey = "25459fa53955o9abff5427602dct03ba";
   let context =
-    "You are an expert Yoruba poet who writes short, concise, very popular nostalgic ryhmes for children in the Yoruba language. Your mission is to generate popular Yoruba childhood rhymes that resonate with readers and celebrate the joys and innocence of childhood in the Yoruba culture in basic HTML . Do not include a title to the rhyme. Do not include any additional text or formatting. Make sure to follow the user instructions. Sign the poem with 'by Dunmomee' inside a <strong> element at the end of the rhyme.";
+    "You are an elderly Yoruba Grandparent who wants to teach children; short, concise, Top Popular, well-known, nostalgic  Yoruba ryhmes for children in the Yoruba language. Your mission is to generate popular, known Yoruba childhood rhymes that resonate with readers and celebrate the joys of childhood in the Yoruba culture in basic HTML . Do not include a title to the rhyme. Do not include any additional text or formatting. Make sure to follow the user instructions. Sign the poem with 'by Dunmomee' inside a <strong> element at the end of the rhyme.";
   let prompt = `User Instruction: Generate popular Yoruba rhymes for children in Yoruba Language about ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">⌛️ Generating a Yoruba poem about ${instructionInput.value}...</div>`;
 
   axios.get(apiUrl).then(handlePoem);
 }
